ac21d5dcac530fc03446ea6366304b2354a66e8f
Hackbright Student <info@hackbrightacademy.com>
2014-02-25 13:02:54 -0800

    first commit with stuff done

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def index():
 def process_login():
     return render_template("login.html")
 
-@app.route("/youshouldprobablychangethisurl")
+@app.route("/register")
 def register():
     return render_template("register.html")
 
