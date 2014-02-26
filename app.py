3aff89e793d3263f27ed4c97430fad3b10c04628
Hackbright Student <info@hackbrightacademy.com>
2014-02-25 16:57:55 -0800

    End of day. Working on wall post functionality.

--- app.py
+++ app.py
@@ -14,17 +14,10 @@ def index():
 
 @app.route("/", methods=["POST"])
 def process_login():
+    model.connect_to_db()
     username = request.form.get("username")
     password = request.form.get("password")
 
-    # username = model.authenticate(username, password)
-    # if username != None:
-    #     flash('message') = "User authenticated!"
-    #     session['username'] = username
-    # else:
-    #     flash('message') = "Password incorrect, there may be a ferret stampede in progress. Save yourself!"
-
-    # return redirect(url_for("index"))
     if model.authenticate(username, password):
         flash("User authenticated")
         session['username'] = username
@@ -44,6 +37,21 @@ def clear_session():
 @app.route("/register")
 def register():
     return render_template("register.html")
+
+@app.route("/user/<username>")
+def user_wall():
+    #take username, look up user_id
+    #to do this, write get_user_by_name func in model.py
+    #call this function
+    #use user_id to look up wall_posts
+    #return posts/rows to wall.html
+    model.connect_to_db()
+    username = request.args.get("username")
+    user_id = model.get_userid_by_name(username)
+    wall_posts = model.get_wall_posts(user_id)
+    html = render_template("wall.html", wall_posts=wall_posts)
+    return html
+
 
 if __name__ == "__main__":
     app.run(debug = True)
--- model.py
+++ model.py
@@ -1,12 +1,56 @@
+import sqlite3
+
+DB = None
+CONN = None
+
 ADMIN_USER="hackbright"
 ADMIN_PASSWORD=5980025637247534551
 
 #803096023
 
 def authenticate(username, password):
-    print hash(password)
-    if username == ADMIN_USER and hash(password) == ADMIN_PASSWORD:
-        return ADMIN_USER
-
+    password_input = hash(password)
+    print password_input
+    query = """SELECT password, id FROM users WHERE username = ?"""
+    DB.execute(query, (username,))
+    row = DB.fetchone()
+    DB_password = hash(row[0])
+    if password_input == DB_password:
+        return row[1]
     else:
         return None
+
+def get_userid_by_name(username):
+    #write function to take username, look up user_id
+    pass
+
+def get_username_by_id(userid):
+    pass
+
+def get_wall_posts(user_id):
+    connect_to_db()
+    query = """SELECT id, content, created_at, author_id FROM wall_posts WHERE owner_id = ? """
+    DB.execute(query, (user_id,))
+    rows = DB.fetchall()
+    posts = {}
+    for row in rows:
+        posts[row[0]] = {"author": row[3], "date": row[2], "content": row[1]}
+    print posts
+    return posts
+  
+
+def connect_to_db():
+    global DB, CONN
+    CONN = sqlite3.connect("thewall.db")
+    DB = CONN.cursor()
+
+def main():
+    connect_to_db()
+    command = None
+    while command != "quit":
+        pass
+
+    CONN.close()
+
+if __name__ == "__main__":
+    main()
